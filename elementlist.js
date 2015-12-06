fe86b3215de9f54edca397824d629033bbf3c288
Travis <travis@travis-ci.org>
2015-12-06 17:50:36 +0000

    API updated

---
+++ elementlist.js
@@ -0,0 +1,3 @@
+
+var ApiGen = ApiGen || {};
+ApiGen.elements = [["c","EmanueleMinotto\\TwigCacheBundle\\DependencyInjection\\Configuration"],["c","EmanueleMinotto\\TwigCacheBundle\\DependencyInjection\\TwigCacheExtension"],["c","EmanueleMinotto\\TwigCacheBundle\\KeyGenerator\\SplObjectHashKeyGenerator"],["c","EmanueleMinotto\\TwigCacheBundle\\Strategy\\ProfilerStrategy"],["c","EmanueleMinotto\\TwigCacheBundle\\Twig\\ProfilerExtension"],["c","EmanueleMinotto\\TwigCacheBundle\\TwigCacheBundle"]];
